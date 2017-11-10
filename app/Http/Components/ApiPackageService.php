023d57b487a1cb892fd20f58668ade49572900ed
Do Ngoc <ducngoc110@gmail.com>
2017-11-10 11:10:29 +0700

    api package service

---
+++ app/Http/Components/ApiPackageService.php
@@ -0,0 +1,66 @@
+<?php
+namespace app\Http\Components;
+use App\Model\BuyModel;
+use App\Model\ServiceModel;
+
+class ApiPackageService
+{
+    /**
+     * $getInstance - Support singleton module.
+     *
+     * @var null
+     */
+    private static $getInstance = null;
+
+    private function __wakeup() {}
+
+    private function __clone( ) {}
+
+    private function __construct() {}
+
+    public static function getInstance()
+    {
+        if (!(self::$getInstance instanceof self)) {
+            self::$getInstance = new self();
+        }
+
+        return self::$getInstance;
+    }
+
+    public function checkStatusPackage( $id ) {
+        $mdBuy = new BuyModel;
+        $mdService = new ServiceModel;
+
+        $status = [];
+        $status['expire'] = true;
+        $status['limit'] = true;
+        $infoBuy = $mdBuy->getBy( 'id', $id );
+
+        $date = $this->countDay( $infoBuy[0]['buy_created'], date("Y-m-d H:i:s") );
+        if ( $date > $infoBuy[0]['buy_date']*30 ) {
+            $status['expire'] = false;
+        }
+
+        $post_limit = $mdService->getMetaData( $infoBuy[0]['buy_packet'], 'post_limit' );
+        $countUsed = ( $infoBuy[0]['buy_used_day'] != date("Y-m-d") ) ? 0 : $infoBuy[0]['buy_run_day'];
+        if ( $countUsed >= $post_limit ) {
+            $status['limit'] = false;
+        }
+
+        return json_encode( $status );
+    }
+
+    public function countDay( $startDate, $endDate ) {
+        $startTimeStamp = strtotime( $startDate );
+        $endTimeStamp = strtotime( $endDate );
+
+        $timeDiff = abs($endTimeStamp - $startTimeStamp);
+
+        $numberDays = $timeDiff/86400;  // 86400 seconds in one day
+
+        // and you might want to convert to integer
+        $numberDays = intval($numberDays);
+
+        return $numberDays;
+    }
+}
